06fb1cd4a3d8adc301e78ea32a1983a4b47585dc
Davi-Ga <davigaldinoky@gmail.com>
2023-02-09 14:43:32 -0300

    calculate

--- item_based.py
+++ item_based.py
@@ -1,3 +1,9 @@
+from search import get_similarities
+
 def calculate_item_similarities(dataset):
     similarities={}
     for item in dataset:
+        similarity=get_similarities(dataset,item)
+        similarities[item]=similarity
+        
+    return similarities
