9fdf74df6a60cab6b27907414f1643eb3e0c3797
Davi-Ga <davigaldinoky@gmail.com>
2023-02-14 10:48:02 -0300

    get item

--- item_based.py
+++ item_based.py
@@ -7,3 +7,21 @@ def calculate_item_similarities(dataset):
         similarities[item]=similarity
         
     return similarities
+
+def get_item_recommendations(dataset,user):
+    totals={}
+    simSums={}
+    item_similarities=calculate_item_similarities(dataset)
+    
+    for (item,rating) in dataset[user].items():
+        for (similarity,item2) in item_similarities[item].items():
+            if item2 in dataset[user]:
+                continue
+            totals.setdefault(item2,0)
+            totals[item2]+=similarity*rating
+            simSums.setdefault(item2,0)
+            simSums[item2]+=similarity
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
